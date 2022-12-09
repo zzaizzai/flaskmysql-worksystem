5cbbe8297f9f82e97c6aa13b04d00ead6e1d4f0f
zzaizzai <junsai0530@gmail.com>
2022-12-10 02:28:43 +0900

    dd

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from app import app
+import os
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    app.run(debug=True, use_reloader=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
